6e722fb2088154a8d282256012c15cca840adb2b
LyManG <lmt294@gmail.com>
2016-03-14 10:13:56 +0100

    20160314_LyManG_ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  InvMat <- NULL
+  set <- function(my) {
+    xm <<- my
+    InvMat <<- NULL
+  }
+  get <- function() xm
+  setsolve <- function(mat=matrix())
+  {mat2<- solve(mat); InvMat<<-mat2; InvMat }
+  getsolve <- function() InvMat
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
@@ -12,4 +24,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		
+  xm <- fx$getsolve()
+  if(!is.null(xm)) {
+    message("getting cached Solve data")
+    return(xm)
+  }
+  data <- fx$get()
+  xm <- solve(data, ...)
+  fx$setsolve(xm)
+  xm
 }
+		
+
